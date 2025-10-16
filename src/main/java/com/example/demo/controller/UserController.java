5cc84221b0c3decce07c9da4f92e0c914a42810f
koi <nuyenkhoi2407@gmail.com>
2025-10-17 00:19:22 +0700

    first commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.UserService;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/123")
+    public String getHomePage(){
+        String test = this.userService.handleHello();
+        return "hehe";
+    }
+}
+
+// @RestController
+// public class UserController {
+    
+//     private UserService userService;
+
+//     public UserController(UserService userService) {
+//         this.userService = userService;
+//     }
+
+//     @GetMapping("/")
+//     public String getHomePage(){
+//         return userService.handleHello();
+//     }
+// }
